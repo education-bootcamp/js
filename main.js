6483a418706aa2eb82c9d378f51eeb527d259795
Sanduni Wijesinghe <73924997+sanduniWijesinghe@users.noreply.github.com>
2022-10-08 12:24:54 +0530

    7

--- main.js
+++ main.js
@@ -1,18 +1,42 @@
 let number1=0;
 let number2=0;
-sum=()=>{
-    calculate("+");
-}
+sum=()=>calculate("+");
+mul=()=>calculate("*");
+div=()=>calculate("/");
+sub=()=>calculate("-");
+mod=()=>calculate("%");
 
 function calculate(exp) {
-    let tempValue=document.
-    getElementById('textInput').value;
-    if (isNaN(tempValue) || tempValue===''){
+    let tempValue=Number(document.
+    getElementById('textInput').value);
+    if (isNaN(tempValue) || tempValue===0){
         alert('Please enter a valid number');
         return;
     }
     if (number1===0){
            number1=tempValue;
+           document.getElementById('textInput').value='';
+           return;
+    }else{
+        number2=tempValue;
+        document.getElementById('textInput').value='';
     }
-    console.log(exp, number1);
+
+    switch (exp) {
+        case '+':
+            console.log(number1+number2);
+            break;
+        case '-':
+            console.log(number1-number2);
+            break;
+        case '*':
+            console.log(number1*number2);
+            break;
+        case '/':
+            console.log(number1/number2);
+            break;
+        case '%':
+            console.log(number1%number2);
+    }
+
 }
